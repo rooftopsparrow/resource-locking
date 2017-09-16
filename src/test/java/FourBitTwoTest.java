8082eae0d7991799e0416b86929bebe6f43c39e6
Jonathan Nicholson <jnicho31@msudenver.edu>
2017-09-16 14:41:28 -0600

    Allow both Device and DevDevice to exist

--- src/test/java/FourBitTwoTest.java
+++ src/test/java/FourBitTwoTest.java
@@ -5,13 +5,29 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.containsString;
 
-
-
 public class FourBitTwoTest {
+
+  static final boolean USEDEVDEVICE = false;
+
+  static Device buildDevice(boolean[] init, int bits) {
+    if (USEDEVDEVICE) {
+      return new DevDevice(init, bits);
+    } else {
+      return new Device(init, bits);
+    }
+  }
+
+  static Device buildDevice() {
+    if (USEDEVDEVICE) {
+      return new DevDevice();
+    } else {
+      return new Device();
+    }
+  }
 
   @Test public void testItUnlocksTheDeviceWithAllTrue() {
     boolean[] init = {true, true, true, true};
-    Device d = new Device(init, 2);
+    Device d = FourBitTwoTest.buildDevice(init, 2);
     assertTrue(FourBitTwoDisclosureDeviceUnlocker.unlock(d));
     String trace = FourBitTwoDisclosureDeviceUnlocker.showTrace();
     assertThat(trace, containsString("SUCCESS"));
@@ -20,7 +36,7 @@ public class FourBitTwoTest {
 
   @Test public void testItUnlocksTheDeviceWithAllFalse() {
     boolean[] init = {false, false, false, false};
-    Device d = new Device(init, 2);
+    Device d = FourBitTwoTest.buildDevice(init, 2);
     assertTrue(FourBitTwoDisclosureDeviceUnlocker.unlock(d));
     String trace = FourBitTwoDisclosureDeviceUnlocker.showTrace();
     assertThat(trace, containsString("SUCCESS"));
@@ -28,7 +44,7 @@ public class FourBitTwoTest {
   }
 
   @Test public void testItUnlocksTheDeviceWhenSetRandomBits() {
-    Device d = new Device();
+    Device d = FourBitTwoTest.buildDevice();
     assertTrue(FourBitTwoDisclosureDeviceUnlocker.unlock(d));
     String trace = FourBitTwoDisclosureDeviceUnlocker.showTrace();
     assertThat(trace, containsString("SUCCESS"));
--- src/test/java/UnfairTestDevice.java
+++ src/test/java/UnfairTestDevice.java
@@ -2,9 +2,11 @@
  * Make spin return false always
  */
 class UnfairTestDevice extends Device {
+  @Override
   public boolean spin() {
    return false;
   }
+  @Override
   public CharSequence peek(CharSequence c) {
     return "FF--";
   }
