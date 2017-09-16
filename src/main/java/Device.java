0b90ced8e0b7c90f3274163673987aabae9fb2ce
Mojac <mp.wowplay@gmail.com>
2017-09-16 07:18:06 -0600

    Created part of the Device class
    I did the skeleton and some documentation (might not have been needed) and the logic for the spin and toString methods for the Device class, it still needs logic for peek and poke. If no one wants to finish those two methods then I'll have time later today to finish them. 
    If anyone does want to finish it, be sure to use rotateOffset so we play around with indexes and don't have to keep swapping the data around. Also, someone might want to check the numbers I used in the code for spin, they seem arbitrary, but are they truly random for this implementation?

---
+++ src/main/java/Device.java
@@ -0,0 +1,150 @@
+ /**
+ * <p>Resource-locking device with circular bit storage.</p>
+ * 
+ * <p><b>Version:</b>
+ * <p>v1.2.5.1
+ * <p><b>Author:</b>
+ * <p>Michael Palme
+ * 
+ * @author Michael Palme
+ * @version 1.2.5.1
+ * 
+ **/
+
+import java.lang.Math;
+
+public class Device
+{
+   /** Default number of bits to reveal per peek. */
+   public static final int DEFAULT_PEEKS = 2;
+   /** Default number of bits stored. */
+   public static final int DEFAULT_SIZE = 4;
+   /** Character indicator of false. */
+   public static final char VALUE_FALSE = 'F';
+   /** Character indicator of true. */
+   public static final char VALUE_TRUE = 'T';
+   
+   private boolean[] bitLockArray; // '1' and '0' bit storage
+   private int rotateOffset, // So spin() doesn't move contents, just indexes
+               numToPeek; // User specified number of bits to look at
+   private String toReturn; // peek() and toString() use this storage
+   
+   /**
+    * Construct device using defaults.
+    */
+   public Device()
+   {
+      this.numToPeek = DEFAULT_PEEKS;
+      this.rotateOffset = 0;
+      this.bitLockArray = new boolean[DEFAULT_SIZE];
+      for(int randomizer = 0; randomizer < DEFAULT_SIZE; randomizer++)
+         this.bitLockArray[randomizer] = Math.random()<0.5? true : false;
+   }//End Constructor #1
+   
+   /**
+    * Construct device with specified bits for testing.
+    * @param initialBits the bit values for this test device
+    * @param bitsPerPeek the number of bits to disclose via peek or set via poke
+    */
+   public Device(boolean[] initialBits, int bitsPerPeek)
+   {
+      this.numToPeek = bitsPerPeek;
+      this.rotateOffset = 0;
+      this.bitLockArray = initialBits;
+   }//End Constructor #2
+   
+   /**
+    * Construct device with specified size and number of peel/poke bits.
+    * @param size the number of bits stored in this device
+    * @param bitsPerPeek the number of bits to disclose via peek or set via poke
+    */
+   public Device(int size, int bitsPerPeek)
+   {
+      this.numToPeek = bitsPerPeek;
+      this.rotateOffset = 0;
+      this.bitLockArray = new boolean[size];
+      for(int randomizer = 0; randomizer < DEFAULT_SIZE; randomizer++)
+         this.bitLockArray[randomizer] = Math.random()<0.5? true : false;
+   }//End Constructor #3
+   
+   
+   
+   /**
+    * Peek at bits of device.
+    * 
+    * @param pattern indicates which bits to reveal as '?'. pattern.length() 
+    *        must be exactly equal to the number of bits stored in the device.
+    * @return a pattern that discloses the values of the indicated btis as 'T' or 'F'
+    */
+   public CharSequence peek(CharSequence pattern)
+   {
+      
+      return pattern;//TODO
+   }//End peek()
+   
+   
+   
+   /**
+    * Poke bits into device.
+    * 
+    * @param pattern indicator of values of bits to poke. pattern.length() 
+    *        must be exactly equal to the number of bits stored in the device. 
+    *        Values of 'T' or 'F' that correspond to '?' in the preceding peek 
+    *        request pattern replace the values in the device.
+    */
+   public void poke(CharSequence pattern)
+   {
+      //TODO
+   }//End poke()
+   
+   
+   
+   /**
+    * Initiate device rotation.
+    * 
+    * @return true if all bits have identical value; false otherwise
+    */
+   public boolean spin()
+   {
+      boolean startIx = this.bitLockArray[0];
+      for(int checker = 1; checker < this.bitLockArray.length; checker++)
+      {
+         if(startIx != this.bitLockArray[checker])
+            break;
+         if(checker == this.bitLockArray.length-1)
+            return true;
+      }
+      
+      this.rotateOffset = Math.round( 
+       (float)(Math.random() * (100.0+this.rotateOffset) * (double)this.numToPeek) 
+                                    ) % this.bitLockArray.length;
+      
+      //System.out.println("DEBUG: Rotate offset = " + rotateOffset);
+      return false;
+   }//End spin()
+   
+   
+   
+   /**
+    * Render device information as a string.
+    * 
+    * {@inheritDoc}
+    * @return rendering that reveals partial state
+    */
+   @Override
+   public String toString()
+   {
+      this.toReturn = "[";
+      for(int printer = 0; printer < this.bitLockArray.length; printer++)
+      {
+         if(this.bitLockArray[printer] == true)
+            this.toReturn += VALUE_TRUE;
+         else
+            this.toReturn += VALUE_FALSE;
+      }
+      this.toReturn += "]";
+      
+      return this.toReturn;
+   }//End toString()
+   
+}//End Device
