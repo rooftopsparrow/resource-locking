2b33ff7bc85972fd76893fd30982abd3b0ebc29a
arcturas1 <arcturas1@gmail.com>
2017-09-17 14:51:47 -0600

    removed extra hyphen from method param

--- src/main/java/FourBitTwoDisclosureDeviceUnlocker.java
+++ src/main/java/FourBitTwoDisclosureDeviceUnlocker.java
@@ -25,7 +25,7 @@ public class FourBitTwoDisclosureDeviceUnlocker extends DeviceUnlocker {
     /**
      *Unlocks a resource controlled by a 4-bit/2-disclosure device. Behavior is unspecified if parameter is not a reference to a valid 4-bit/2-disclosure device.
      *
-     * @param  dev - the device controlling the resource to unlock; must be a 4-bit device with 2 peek/poke bits.
+     * @param  dev the device controlling the resource to unlock; must be a 4-bit device with 2 peek/poke bits.
      * @return  true if the resource is successfully unlocked (all bits are now identical); false otherwise
      */
     public static boolean unlock(Device dev){
